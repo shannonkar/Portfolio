702b38d5bad4df085337fcbda4d2168a12b4980e
Shannon Makenna <shannonkariuki@gmail.com>
2020-08-16 11:37:41 -0400

    Updates

---
+++ precache-manifest.835b00e8c0225d4e8cbeef26e61984ed.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7fcaf6254b8d239b7a2722e62d26325",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "e10ab2913acbd84fb7a7",
+    "url": "/Portfolio/static/css/main.0d7714cc.chunk.css"
+  },
+  {
+    "revision": "091e0b00110bc4920872",
+    "url": "/Portfolio/static/js/2.f327f5e1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Portfolio/static/js/2.f327f5e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e10ab2913acbd84fb7a7",
+    "url": "/Portfolio/static/js/main.a4a9758e.chunk.js"
+  },
+  {
+    "revision": "d5aa6f7e9aca8eb23b32",
+    "url": "/Portfolio/static/js/runtime-main.42c10d41.js"
+  },
+  {
+    "revision": "75ab9194564675d0ae0fc48fd2002808",
+    "url": "/Portfolio/static/media/Shannon M. Kariuki_Resume.75ab9194.pdf"
+  },
+  {
+    "revision": "0a9813fcf60fb6dd059aeacbd503980f",
+    "url": "/Portfolio/static/media/css.0a9813fc.svg"
+  },
+  {
+    "revision": "17efd8231a2f556f14b18057577dfd7e",
+    "url": "/Portfolio/static/media/faceRecognition.17efd823.png"
+  },
+  {
+    "revision": "f0361c5e9bebc1c5647523db385641cf",
+    "url": "/Portfolio/static/media/html5.f0361c5e.svg"
+  },
+  {
+    "revision": "bcfad0f2c3e9c170d9e85e09a061d650",
+    "url": "/Portfolio/static/media/java.bcfad0f2.svg"
+  },
+  {
+    "revision": "2ba36cb496576096e0080f3f81996f97",
+    "url": "/Portfolio/static/media/jumpiana.2ba36cb4.png"
+  },
+  {
+    "revision": "889698514e5b35b4a4617497e195bf37",
+    "url": "/Portfolio/static/media/moodify.88969851.png"
+  },
+  {
+    "revision": "f80a91ba03fbf0e99ea6949aa3a6399e",
+    "url": "/Portfolio/static/media/mysql.f80a91ba.svg"
+  },
+  {
+    "revision": "e3987c7499d8111d615da5262bc83cb8",
+    "url": "/Portfolio/static/media/portfolio.e3987c74.png"
+  },
+  {
+    "revision": "d944a3cafad397f6f1d083cf791f7233",
+    "url": "/Portfolio/static/media/profilepic.d944a3ca.png"
+  },
+  {
+    "revision": "05a3e30fdf60ebe9d00cf4725d44bd42",
+    "url": "/Portfolio/static/media/react.05a3e30f.svg"
+  },
+  {
+    "revision": "aa0a337603611296524a666b62a9ff23",
+    "url": "/Portfolio/static/media/redux.aa0a3376.svg"
+  },
+  {
+    "revision": "d763ac668d3a135696e198421c8da82f",
+    "url": "/Portfolio/static/media/steamlabke.d763ac66.png"
+  },
+  {
+    "revision": "3de182d2120823fe49c27eeb1765c53a",
+    "url": "/Portfolio/static/media/typescript.3de182d2.svg"
+  }
+]);
